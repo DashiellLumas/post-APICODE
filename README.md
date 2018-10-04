30417c683f7314fb84a899441d294ac038d094ce
DashiellLumas <dashiell.lumas@gmail.com>
2018-10-04 15:34:10 -0400

    code for challenge

---
+++ README.md
@@ -0,0 +1,29 @@
+# Kustomer Coding Challenge
+&nbsp;
+## Setup Instructions
+&nbsp;
+##### Download zip file
+&nbsp;
+##### Download jq
+Windows Users: see https://stedolan.github.io/jq/download/
+Mac Users: (Install Homebrew)
+``
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+``
+&nbsp;
+``
+brew install jq
+``
+&nbsp;
+##### Next
+&nbsp;
+##### run ``npm install``
+&nbsp;
+##### Next
+&nbsp;
+To convert csv to json:
+##### run ``jq -R -s -f -j csv2json.jq data.csv > output.json``
+&nbsp;
+##### Next
+To post object(s) to Kustomer API:
+###### run ``npm run post ``
---
+++ post.js
@@ -0,0 +1,83 @@
+require('dotenv').config();
+const http = require("https");
+const API_KEY = process.env.API_KEY;
+const API_HOST = process.env.API_HOST;
+const API_PATH = process.env.API_PATH;
+
+import * as obj from './output.json';
+
+try{
+  formatAPIObjects(obj);
+}catch(error){
+}
+
+function formatAPIObjects(object){
+  Object.keys(object).forEach( (arrayItem,index) => {
+    // no null values allowed by API for these properties
+    if(object.default[index].email != "" && object.default[index].homePhone != "" && object.default[index].workPhone != "" ){
+
+      // prepare API OBJ
+     const API_OBJ = {
+        "name": null,
+        "emails": [
+         {
+           "type": "home",
+           "email": null
+         }
+        ],
+        "phones": [
+         {
+           "type": "home",
+           "phone": null
+         },
+         {
+           "type": "work",
+           "phone": null
+         }
+        ]
+        }
+  //Format for API
+    API_OBJ.name = object.default[index].firstName + " " + object.default[index].lastName;
+    API_OBJ.emails[0].email = object.default[index].email
+    API_OBJ.phones[0].phone = object.default[index].homePhone.replace(/\./g, "-")
+    API_OBJ.phones[1].phone = object.default[index].workPhone.replace(/\./g, "-")
+    let data = JSON.stringify(API_OBJ)
+
+
+   // Post data object
+   postObjectToAPI(data);
+
+}
+
+
+
+function postObjectToAPI(data){
+  let options = {
+ "method": "POST",
+ "hostname": API_HOST,
+ "port": null,
+ "path": API_PATH,
+ "headers": {
+   "authorization": `Bearer ${API_KEY}`,
+   "content-type": "application/json"
+ }
+};
+
+let req = http.request(options, (res) => {
+ let chunks = [];
+
+ res.on("data",  (chunk) => {
+   chunks.push(chunk);
+ });
+
+ res.on("end",  () => {
+   let body = Buffer.concat(chunks);
+   console.log(body.toString());
+ });
+});
+
+req.write(data);
+req.end();
+}
+ })
+}
